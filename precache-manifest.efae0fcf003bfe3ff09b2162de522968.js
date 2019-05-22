649fa641170b5570cf46de3bcc9bfb71e0e2b852
gabriel <duartega@sonoma.edu>
2019-05-22 00:01:34 -0700

    Updates

---
+++ precache-manifest.efae0fcf003bfe3ff09b2162de522968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a17372921132e3e24e88",
+    "url": "/static/js/main.a1737292.chunk.js"
+  },
+  {
+    "revision": "1d1413016ba5b5942f9e",
+    "url": "/static/js/2.1d141301.chunk.js"
+  },
+  {
+    "revision": "a17372921132e3e24e88",
+    "url": "/static/css/main.62635d23.chunk.css"
+  },
+  {
+    "revision": "188f0b2238b885d5bd1f944016a88665",
+    "url": "/index.html"
+  }
+];

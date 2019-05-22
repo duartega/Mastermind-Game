9ecc2ed564677cdf0313bb8ec0f8b9167b6b2265
gabriel <duartega@sonoma.edu>
2019-05-22 00:12:03 -0700

    Updates

---
+++ precache-manifest.0a88aeb32686b60067608da22620d2ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d72fad506a03b739dc2",
+    "url": "/Mastermind-Game/static/js/runtime~main.5d72fad5.js"
+  },
+  {
+    "revision": "6ad30d570a661fbc1710",
+    "url": "/Mastermind-Game/static/js/main.6ad30d57.chunk.js"
+  },
+  {
+    "revision": "1d1413016ba5b5942f9e",
+    "url": "/Mastermind-Game/static/js/2.1d141301.chunk.js"
+  },
+  {
+    "revision": "6ad30d570a661fbc1710",
+    "url": "/Mastermind-Game/static/css/main.62635d23.chunk.css"
+  },
+  {
+    "revision": "5e26f3e10994dba82c4215558e462f5d",
+    "url": "/Mastermind-Game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efae0fcf003bfe3ff09b2162de522968.js"
+  "/Mastermind-Game/precache-manifest.0a88aeb32686b60067608da22620d2ec.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Mastermind-Game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
